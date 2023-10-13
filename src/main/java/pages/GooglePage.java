2b92d283bc8b1fb6c62f4f4287381c76af69d2fd
Danny-Ixcaya <15daixca@gmail.com>
2023-10-13 05:08:28 -0600

    Creacion de registro de ususario, para poder realizar ocomorap, ingreso de tarjeta no funciona

--- src/main/java/pages/GooglePage.java
+++ src/main/java/pages/GooglePage.java
@@ -1,12 +1,13 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,16 +18,82 @@ public class GooglePage extends BasePage{
 
 
     public void navigateToGoogle(){
-        navigateTo("https://heggerty.org/my-account/");
+        navigateTo("https://heggerty.org/user-registration/");
+        //https://heggerty.org/my-account/
+    }
+
+
+    public void Register(String idInput, String valueInput){
+        WebElement inputEnter = driver.findElement(By.id(idInput));
+        slowType(inputEnter, valueInput);
+    }
+
+    public void SelectProffession(){
+        // seleccionar el elemento por su id
+        WebElement selectElement = driver.findElement(By.id("input_11_4"));
+
+        Select select = new Select(selectElement);
+
+        select.selectByValue("specialist_teacher_coach");
+    }
+
+    public void RadioButton(){
+        WebElement labelElement = driver.findElement(By.cssSelector("label[for='choice_11_22_4']"));
+
+        labelElement.click();
+    }
+
+    public void RadiobtTwo(){
+        WebElement labelElement = driver.findElement(By.cssSelector("label[for='choice_11_21_0']"));
+
+        labelElement.click();
+    }
+
+    public void RegitroButon(){
+        WebElement submitButton = driver.findElement(By.id("gform_submit_button_11"));
+
+        submitButton.click();
+        try {
+            Thread.sleep(10000); // Pausa de 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void IrIniciarSesion(){
+        WebElement divElement = driver.findElement(By.className("fl-module-button"));
+        WebElement anchorElement = divElement.findElement(By.tagName("a"));
+
+        anchorElement.click();
+
+        try {
+            Thread.sleep(10000); // Pausa de 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void EnterCredentials(String idInput, String valueInput){
         WebElement inputEnter = driver.findElement(By.id(idInput));
         slowType(inputEnter, valueInput);
+
+        // Agrega una espera de 3 segundos después de ingresar los datos
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     public void login() {
         WebElement botonLogin = driver.findElement(By.name("login"));
         botonLogin.click();
+
+        try {
+            Thread.sleep(10000); // Pausa de 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
@@ -35,6 +102,13 @@ public class GooglePage extends BasePage{
 
         // Hacer clic en el enlace
         enlaceShop.click();
+        // Agrega una espera de 3 segundos después de ingresar los datos
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
@@ -42,30 +116,78 @@ public class GooglePage extends BasePage{
 
         WebElement shopDLink = driver.findElement(By.linkText("SHOP ALL PRODUCTS"));
         shopDLink.click();
+        // Agrega una espera de 3 segundos después de ingresar los datos
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void seleccionar() {
-        // Encontrar el enlace del producto por su atributo "href"
         WebElement enlaceProducto = driver.findElement(By.cssSelector("a[href='?add-to-cart=1289672']"));
 
-        // Hacer clic en el enlace del producto
         enlaceProducto.click();
+        // Agrega una espera de 3 segundos después de ingresar los datos
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void Car(){
         WebElement botonCart = driver.findElement(By.id("cart"));
         botonCart.click();
+        // Agrega una espera de 3 segundos después de ingresar los datos
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //WebElement link = driver.findElement(By.linkText("View cart"));
+        //link.click();
+
     }
 
     public void Check(){
-        // Encontrar la etiqueta por el texto "Proceed to checkout"
+        // Encontrar la etiqueta por el texto Proceed to checkout
         WebElement etiquetaCheckout = driver.findElement(By.linkText("Proceed to checkout"));
         etiquetaCheckout.click();
+        try {
+            Thread.sleep(10000); // Pausa de 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void SelecCalle(){
+        List<WebElement> divElements = driver.findElements(By.cssSelector("div.pcaitem"));
+
+        // Valor por defecto del seleccionador
+        String valorPorDefecto = "2780 Tapo Canyon Rd Unit ADDRESS";
+
+        for (WebElement divElement : divElements) {
+            String texto = divElement.getText();
+            //System.out.println("Valor del div: " + texto);
+
+            // Si el texto coincide con el valor por defecto, selecciona el elemento
+            if (texto.equals(valorPorDefecto)) {
+                divElement.click();
+                break;
+            }
+        }
+        try {
+            Thread.sleep(10000); // Pausa de 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void RegisterInfo(String Company, String AdressOne, String City, String Code, String Phone){
 
-        // Captura el elemento label del radio button "Commercial"
+
+        // Captura el elemento label del radio button Commercial
         WebElement commercialLabel = driver.findElement(By.xpath("//label[@for='ak_addr_type_2']"));
         WebElement inCompany = driver.findElement((By.id("billing_company")));
         WebElement inaddressOne = driver.findElement((By.id("billing_address_1")));
@@ -85,32 +207,35 @@ public class GooglePage extends BasePage{
         slowType(inaddressOne, AdressOne);     sleep(3000);//direccion 1
         slowType(inCity, City);     sleep(3000);//para ciudad
 
-        // Crea un objeto Select para estados//
+
+        // objeto Select para paises//
+        Select selectCountry = new Select(selectElementCountry);//
+        // Selecciona una opción por valor ("Guatemala")//
+        selectCountry.selectByValue("GT");    sleep(3000);//
+
+
+        // objeto Select para estados//
         Select select = new Select(selectElement);//
-        // Selecciona una opción por valor (por ejemplo, "Alabama")//
-        select.selectByValue("AL");     sleep(3000);//
+        // Selecciona una opción por valor ("Solola")//
+        select.selectByValue("SO");     sleep(3000);//
 
         slowType(inCode, Code);     sleep(3000);//para el codigo de pais
 
-        // Crea un objeto Select para paises//
-        Select selectCountry = new Select(selectElementCountry);//
-        // Selecciona una opción por valor (por ejemplo, "Alabama")//
-        selectCountry.selectByValue("GT");    sleep(3000);//
 
         slowType(inPhone, Phone);    sleep(3000);//el telefono
     }
 
-    public void RegisterCard(String number){
+    public void RegisterCard(){//esto no funciona
+        WebElement iframe = driver.findElement(By.id("__privateStripeFrame2887"));
 
-        WebElement inNumber = driver.findElement(By.cssSelector("div.p-Input input#Field-numberInput"));
 
-        //WebElement inNumber = driver.findElement((By.id("Field-numberInput")));
-        //WebElement inExpiration = driver.findElement((By.id("Field-expiryInput")));
-        //WebElement inCvc = driver.findElement((By.id("Field-cvcInput")));
+        driver.switchTo().frame(iframe);
 
-        slowType(inNumber, number);     sleep(3000);//para el codigo de pais
-        //slowType(inExpiration, expiration);     sleep(3000);//para el codigo de pais
-        //slowType(inCvc, Cvc);     sleep(3000);//para el codigo de pais
+        WebElement inputElement = driver.findElement(By.id("Field-numberInput"));
+
+        inputElement.clear();
+
+        inputElement.sendKeys("5100010000000114");
 
     }
 
